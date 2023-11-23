91ce6f11b095eac6a3d2f92f1ed2ca4aa9b4f505
MiTange <tmrevo210@yahoo.co.jp>
2023-11-23 22:46:35 +0900

    janome

--- app.py
+++ app.py
@@ -1,30 +1,25 @@
 import streamlit as st
-import MeCab
 import jaconv
 import random
+from janome.tokenizer import Tokenizer
 
 st.title("読みにくい文章ジェネレーター")
 with st.form(key="generate form"):
     option = st.selectbox("出力方法",("スペース区切り","一続き"))
     kugiri = " " if option == "スペース区切り" else ""
-    text = st.text_input("文を入力してね")
+    text = st.text_area("文を入力してね")
     output_button = st.form_submit_button("よくわからんくする")
     
     if output_button:
-        m = MeCab.Tagger()
-        node = m.parseToNode(text)
+        t = Tokenizer()
         words = []
-        while node:
-            feature = node.feature.split(",")
-            if len(feature) == 6 or feature[0] =="補助記号": #英単語と記号
-                words.append(node.surface)
-            elif len(feature) > 19: #日本語
-                hira = jaconv.kata2hira(feature[20])
-                if len(hira) > 2:
-                    hira_naka = hira[1:]
-                    hira = hira[0] + "".join(random.sample(hira_naka, len(hira_naka)))
-                words.append(hira)
-            node = node.next
+        for token in t.tokenize(text):
+            if token.part_of_speech[0] == "記号" or token.reading == "*":
+                words.append(token.surface)
+            else:
+                hira = jaconv.kata2hira(token.reading)
+                hira_shuffle = hira[0] + "".join(random.sample(hira[1:], len(hira[1:])))
+                words.append(hira_shuffle)
         sentence = kugiri.join(words)
 
         st.write(sentence)
